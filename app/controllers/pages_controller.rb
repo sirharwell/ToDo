19ba7f5eae62aa5f61321f41ea39e2b16b49d872
Ian Harwell <sirharwell@gmail.com>
2018-02-26 14:53:02 -0700

    structure

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,55 @@
+class PagesController < ApplicationController
+  #GET /pages
+  def index
+    @pages = Page.all
+  end
+
+  #GET /pages/:id
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  #GET /pages/new
+  def new
+    @page = Page.new
+  end
+
+  #GET /pages/:id/edit
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  #POST /pages
+  def create
+    @page = Page.new(page_params)
+
+    if @page.save
+      redirect_to page_path(@page)
+    else
+      render :new
+    end
+  end
+ 
+  # PUT / PATCH /pages/:id
+  def update
+    @page = Page.find(params[:id])
+
+    if @page.update(page_params)
+      redirect_to page_path(@page)
+    else
+      render :edit
+    end
+  end
+
+  #DELETE /pages/:id
+  def destroy
+    Page.find(params[:id]).destroy
+    redirect_to pages_path
+  end
+
+  private
+    def page_params
+      params.require(:page).permit(:body, :author, :title)
+    end
+end
+
